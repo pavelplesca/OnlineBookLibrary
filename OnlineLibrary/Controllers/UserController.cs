06b23426e577f5709e55c22cce9a384ad12a4aaa
Nichita c <nichitacebotari0@gmail.com>
2018-04-19 15:39:06 +0300

    added user controller

---
+++ OnlineLibrary/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineLibrary.Controllers
+{
+    public class UserController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("User/Authentication")]
+        public ActionResult Authentication()
+        {
+            return View();
+        }
+    }
+}
