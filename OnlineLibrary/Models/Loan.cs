cc80e72b35602a8dbfbcab720893e6f78e4587d2
Cristian Nirones <cristian.nirones@ati.utm.md>
2018-04-19 08:25:15 +0300

    Basic Loan Model created

---
+++ OnlineLibrary/Models/Loan.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OnlineLibrary.Models
+{
+    public enum Status
+    {
+        Active,
+        Canceled,
+        Violated
+    }
+
+    public class Loan
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public DateTime ReturnDate { get; set; }
+
+        [Required]
+        public DateTime BorrowDate { get; set; }
+
+        [Required]
+        public DateTime ReturnedDate { get; set; }
+
+        [Required]
+        public Status Status { get; set; }
+
+        [Required]
+        public int BookID { get; set; }
+        
+        [Required]
+        public Book Book { get; set; }
+    }
+}
