5991a3ad80a90ec7876762210ec7d7800f2f9bc1
Alexandr Djundiet <adjundiet@gmail.com>
2018-04-19 17:11:32 +0300

    Fixed method Seed in migration configuration file

--- OnlineLibrary/Migrations/Configuration.cs
+++ OnlineLibrary/Migrations/Configuration.cs
@@ -24,120 +24,133 @@ namespace OnlineLibrary.Migrations
             context.Tags.AddOrUpdate(new Tag() {Name = "MVC" });
             context.Tags.AddOrUpdate(new Tag() {Name = "ASP.Net" });
             context.Tags.AddOrUpdate(new Tag() {Name = "Programming" });
-            context.Tags.AddOrUpdate(new Tag() {Name = "Artistic" });
-
-
+            context.Tags.AddOrUpdate(new Tag() {Name = "Artistic", });
+            
             context.Books.AddOrUpdate(new Book
             {
-                Id = 1,
+                
                 Name = "The Dark Tower",
                 Author = "Stephen King",
                 Description = "Blablabla",
                 Image = "DarkTower1.jpg",
-                Year = 2018,
-                Tags = new List<Tag>()
-                {
-                    context.Tags.Find(6)
-                }
+                Year = 2018
             });
-
+            
+           
             context.Books.AddOrUpdate(new Book
             {
-                Id = 2,
                 Name = "Harry Potter and the sorcerer's stone",
                 Author = "J. K. Rolling",
                 Description = "Blablabla",
                 Image = "HarryPotter1.jpg",
-                Year = 2018,
-                Tags = new List<Tag>()
-                {
-                    context.Tags.Find(6)
-                }
+                Year = 2018
             });
+           
+
+
             context.Books.AddOrUpdate(new Book
             {
-                Id = 3,
+                
                 Name = "Brief Story of Time",
                 Author = "Stephen Hawking",
                 Description = "Blablabla",
                 Image = "a_brief_history_of_time.jpg",
-                Year = 2018,
-                Tags = new List<Tag>()
-                {
-                    context.Tags.Find(6)
-                }
+                Year = 2018
             });
+            
+
             context.Books.AddOrUpdate(new Book
             {
-                Id = 4,
+                
                 Name = "Game Of Thrones",
                 Author = "G. R. R. Martin",
                 Description = "Blablabla",
                 Image = "GameOfThrones.jpg",
-                Year = 2018,
-                Tags = new List<Tag>()
-                {
-                    context.Tags.Find(6)
-                }
+                Year = 2018
             });
 
+            
             context.Books.AddOrUpdate(new Book
             {
-                Id = 5,
+                
                 Name = "Ready Player One",
                 Author = "Ernest Cline",
                 Description = "Blablabla",
                 Image = "ReadyPlayerOne.jpg",
-                Year = 2018,
-                Tags = new List<Tag>()
-                {
-                    context.Tags.Find(6)
-                }
+                Year = 2018
             });
+            
+
 
             context.Books.AddOrUpdate(new Book
             {
-                Id = 6,
+                
                 Name = "C# In A Nutshell",
                 Author = "Joseph Albahari",
                 Description = "Blablabla",
                 Image = "CSharpInANutshell.jpg",
-                Year = 2018,
-                Tags = new List<Tag>()
-                {
-                    context.Tags.Find(5)
-                }
+                Year = 2018
             });
+           
+
 
             context.Books.AddOrUpdate(new Book
             {
-                Id = 7,
+                
                 Name = "Java In A Nutshell",
                 Author = "Author A. B.",
                 Description = "Blablabla",
                 Image = "java.jpg",
-                Year = 2018,
-                Tags = new List<Tag>()
-                {
-                    context.Tags.Find(5),
-                    context.Tags.Find(1)
-                }
+                Year = 2018
             });
+            
 
             context.Books.AddOrUpdate(new Book
             {
-                Id = 8,
                 Name = "Asp.NET 4",
                 Author = "John Smith",
                 Description = "Blablabla",
                 Image = "aspnet.jpg",
-                Year = 2018,
-                Tags = new List<Tag>()
-                {
-                    context.Tags.Find(5),
-                    context.Tags.Find(4)
-                }
+                Year = 2018
             });
+
+            context.SaveChanges();
+            //--------- Adding Tags in Books
+            context.Books.FirstOrDefault(b => b.Name == "The Dark Tower").Tags.Add(
+                context.Tags.FirstOrDefault(t => t.Name == "Artistic"));
+
+            context.Books.FirstOrDefault(b => b.Name == "Harry Potter and the sorcerer's stone").Tags.Add(
+                context.Tags.FirstOrDefault(t => t.Name == "Artistic")
+            );
+            context.Books.FirstOrDefault(b => b.Name == "Brief Story of Time").Tags.Add(
+                context.Tags.FirstOrDefault(t => t.Name == "Artistic")
+            );
+
+            context.Books.FirstOrDefault(b => b.Name == "Game Of Thrones").Tags.Add(
+                context.Tags.FirstOrDefault(t => t.Name == "Artistic")
+            );
+
+            context.Books.FirstOrDefault(b => b.Name == "C# In A Nutshell").Tags.Add(
+                context.Tags.FirstOrDefault(t => t.Name == "Programming")
+            );
+
+            context.Books.FirstOrDefault(b => b.Name == "Java In A Nutshell").Tags.Add(
+                context.Tags.FirstOrDefault(t => t.Name == "Programming")
+            );
+            context.Books.FirstOrDefault(b => b.Name == "Java In A Nutshell").Tags.Add(
+                context.Tags.FirstOrDefault(t => t.Name == "Java")
+            );
+            context.Books.FirstOrDefault(b => b.Name == "Asp.NET 4").Tags.Add(
+                context.Tags.FirstOrDefault(t => t.Name == "Programming")
+            );
+            context.Books.FirstOrDefault(b => b.Name == "Asp.NET 4").Tags.Add(
+                    context.Tags.FirstOrDefault(t => t.Name == "ASP.Net")
+            );
+            context.Books.FirstOrDefault(b => b.Name == "Ready Player One").Tags.Add(
+                context.Tags.FirstOrDefault(t => t.Name == "Artistic")
+            );
+
+
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
         }
