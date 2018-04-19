4195b3d165e0abda8c2cdb0755d7fe668d87d99b
Cristian Nirones <cristian.nirones@ati.utm.md>
2018-04-19 17:02:53 +0300

    Added a repository for Loan model

---
+++ OnlineLibrary/Persistence/Repositories/LoanRepository.cs
@@ -0,0 +1,24 @@
+﻿using OnlineLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineLibrary.Persistence.Repositories
+{
+    public class LoanRepository
+    {
+        private readonly OnlineLibraryDb context;
+
+        public LoanRepository()
+        {
+            context = new OnlineLibraryDb();
+        }      
+
+        public void DisposeAndSave()
+        {
+            context.SaveChanges();
+            context.Dispose();
+        }
+    }
+}
