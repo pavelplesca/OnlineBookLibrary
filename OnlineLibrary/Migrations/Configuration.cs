d579a1664c64aa6556e40be5e1c0b311805f3cf4
Cristian Nirones <cristian.nirones@ati.utm.md>
2018-04-19 11:33:07 +0300

    Updated Seed method for db init

--- OnlineLibrary/Migrations/Configuration.cs
+++ OnlineLibrary/Migrations/Configuration.cs
@@ -1,6 +1,8 @@
 namespace OnlineLibrary.Migrations
 {
+    using OnlineLibrary.Models;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -15,10 +17,121 @@ namespace OnlineLibrary.Migrations
 
         protected override void Seed(OnlineLibrary.Models.OnlineLibraryDb context)
         {
-            //  This method will be called after migrating to the latest version.
+            context.Books.AddOrUpdate(
+                new Book
+                {
+                    Id = 1,
+                    Name = "The Dark Tower",
+                    Author = "Stephen King",
+                    Description = "Blablabla",
+                    Image = "DarkTower1.jpg",
+                    Year = 2018,
+                    Tags = new List<Tag>()
+                        {
+                            context.Tags.Find(6)
+                        }
+                },
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data.
+                new Book
+                {
+                    Id = 2,
+                    Name = "Harry Potter and the sorcerer's stone",
+                    Author = "J. K. Rolling",
+                    Description = "Blablabla",
+                    Image = "HarryPotter1.jpg",
+                    Year = 2018,
+                    Tags = new List<Tag>()
+                        {
+                            context.Tags.Find(6)
+                        }
+                },
+
+                new Book
+                {
+                    Id = 3,
+                    Name = "Brief Story of Time",
+                    Author = "Stephen Hawking",
+                    Description = "Blablabla",
+                    Image = "a_brief_history_of_time.jpg",
+                    Year = 2018,
+                    Tags = new List<Tag>()
+                    {
+                        context.Tags.Find(6)
+                    }
+                },
+
+                new Book
+                {
+                    Id = 4,
+                    Name = "Game Of Thrones",
+                    Author = "G. R. R. Martin",
+                    Description = "Blablabla",
+                    Image = "GameOfThrones.jpg",
+                    Year = 2018,
+                    Tags = new List<Tag>()
+                    {
+                        context.Tags.Find(6)
+                    }
+                },
+
+                new Book
+                {
+                    Id = 5,
+                    Name = "Ready Player One",
+                    Author = "Ernest Cline",
+                    Description = "Blablabla",
+                    Image = "ReadyPlayerOne.jpg",
+                    Year = 2018,
+                    Tags = new List<Tag>()
+                    {
+                        context.Tags.Find(6)
+                    }
+                },
+
+                new Book
+                {
+                    Id = 6,
+                    Name = "C# In A Nutshell",
+                    Author = "Joseph Albahari",
+                    Description = "Blablabla",
+                    Image = "CSharpInANutshell.jpg",
+                    Year = 2018,
+                    Tags = new List<Tag>()
+                    {
+                        context.Tags.Find(5)
+                    }
+                },
+
+                new Book
+                {
+                    Id = 7,
+                    Name = "Java In A Nutshell",
+                    Author = "Author A. B.",
+                    Description = "Blablabla",
+                    Image = "java.jpg",
+                    Year = 2018,
+                    Tags = new List<Tag>()
+                    {
+                        context.Tags.Find(5),
+                        context.Tags.Find(1)
+                    }
+                },
+
+                new Book
+                {
+                    Id = 8,
+                    Name = "Asp.NET 4",
+                    Author = "John Smith",
+                    Description = "Blablabla",
+                    Image = "aspnet.jpg",
+                    Year = 2018,
+                    Tags = new List<Tag>()
+                    {
+                        context.Tags.Find(5),
+                        context.Tags.Find(4)
+                    }
+                }
+                );
         }
     }
 }
