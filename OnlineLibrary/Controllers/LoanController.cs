5a8cf6232f29c24d7bd897243a20aa44af29bd05
Cristian Nirones <cristian.nirones@ati.utm.md>
2018-04-20 08:44:02 +0300

    Added a method that loads user's loan history into specific partials

--- OnlineLibrary/Controllers/LoanController.cs
+++ OnlineLibrary/Controllers/LoanController.cs
@@ -23,14 +23,33 @@ namespace OnlineLibrary.Controllers
             return View();
         }
 
-        public ActionResult DisplayLoans()
-        {           
+        public ActionResult DisplayLoans(int? userID)
+        {       
             return View();
         }
 
+        [ChildActionOnly]
+        public ActionResult DisplayHistory(int? userID)
+        {
+            if (userID.HasValue)
+            {
+                ICollection<Loan> loanHistory = loanRepository.ReturnLoanHistory(userID.Value).ToList();
+
+                if(loanHistory.Count != 0)
+                {
+                    return PartialView("LoanHistoryPartial", loanHistory);
+                }
+
+                return PartialView("EmptyHistoryPartial");
+            }
+
+            return View("_Error");            
+        }
+
+        [ChildActionOnly]
         public ActionResult CreateLoan(int? bookID, int? userID)
         {
-            if(bookID != null && userID != null)
+            if(bookID.HasValue && userID.HasValue)
             {
                 loanRepository.CreateLoan(bookID.Value, userID.Value);
                 loanRepository.SaveAndDispose();
@@ -40,9 +59,10 @@ namespace OnlineLibrary.Controllers
             return View("_Error");
         }
 
+        [ChildActionOnly]
         public ActionResult CancelLoan(int? bookID, int? userID)
         {
-            if (bookID != null && userID != null)
+            if (bookID.HasValue && userID.HasValue)
             {
                 loanRepository.CancelLoan(bookID.Value, userID.Value);
                 loanRepository.SaveAndDispose();
