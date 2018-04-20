018244f929e92052f19db06699f0f2139ea62e9e
Alexandr Djundiet <adjundiet@gmail.com>
2018-04-20 17:28:33 +0300

    Some code for implementing identity, is not stable.

---
+++ OnlineLibrary/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace OnlineLibrary.Models
+{
+    public class User: IdentityUser
+    {
+        public DateTime BannedUntil { get; set; }
+
+        public User()
+        {
+
+        }
+        
+    }
+}
---
+++ OnlineLibrary/Startup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using OnlineLibrary.Models;
+using Owin;
+
+[assembly: OwinStartup(typeof(OnlineLibrary.Startup))]
+
+namespace OnlineLibrary
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+
+            app.CreatePerOwinContext<OnlineLibraryDb>(OnlineLibraryDb.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/User/Login")
+            });
+        }
+    }
+}
