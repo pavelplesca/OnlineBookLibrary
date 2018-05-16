3070da212e6608623caf0f595cc7b815bd364793
Alexandr Djundiet <adjundiet@gmail.com>
2018-05-16 11:55:14 +0300

    Deleted logging in BookController

--- OnlineBookLibrary/Controllers/BookController.cs
+++ OnlineBookLibrary/Controllers/BookController.cs
@@ -17,25 +17,21 @@ namespace OnlineBookLibrary.Controllers
 {
     public class BookController : Controller
     {
-        Logger logger = new Logger("BookController");
         private readonly BookRepository _bookRepository = new BookRepository();
 
         public BookController()
         {
-            logger.Log("Book Controller started:");
         }
         
         // GET: Book
         public ActionResult Index()
         {
-            logger.Log("Index started");
             ViewBag.Page = 1;
             return View();
         }
         
         public ActionResult BookDetails(int? id)
         {
-            logger.Log("BookDetails started");
             if (id == null)
             {
                 return RedirectToAction("Index", "Home");
@@ -45,54 +41,44 @@ namespace OnlineBookLibrary.Controllers
             {
                 return RedirectToAction("Index", "Home");
             }
-
-            logger.Log("BookDetails return View(book)");
             return View(book);
         }
 
         [ChildActionOnly]
         public ActionResult TopLoans()
         {
-            logger.Log("TopLoans started");
             IEnumerable<Book> books = _bookRepository.GetTopLoanedBooks();
-            logger.Log("TopLoans  return PartialView");
             return PartialView("_TopLoans", books);
         }
 
         public ActionResult TaggedBookPage(int page, string tag)
         {
-            logger.Log("TaggedBookPage started");
             if (tag == null)
                 tag = "All";
             var books = _bookRepository.GetBooksByTag(tag);
             books = _bookRepository.GetPageOfBooks(books, page);
             int maxPage = (books.Count() / 6) + 1;
             ViewBag.maxpage = maxPage;
-            logger.Log("TaggedBookPage return PartialView(_BookPage)");
-
+            
             return PartialView("_BookPage", books);
         }
 
         [ChildActionOnly]
         public ActionResult ReturnTags()
         {
-            logger.Log("ReturnTags started");
             var tags = _bookRepository.GetTags();
-            logger.Log("ReturnTags PartialView (_TagDropdown)");
             return PartialView("_TagDropdown", tags);
         }
 
         [ChildActionOnly]
         public ActionResult DisplayButtons(string userId, Book book)
         {
-            logger.Log("DisplayButtons started");
             var um = HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
             
             User user = um.FindById(userId); 
 
             if (!user.IsBanned)
             {
-                logger.Log("DisplayButtons PartialView(_UserNotBannedPartial)");
                 return PartialView("_UserNotBannedPartial", book);
             }
             return new EmptyResult();
@@ -104,14 +90,7 @@ namespace OnlineBookLibrary.Controllers
             {
                 _bookRepository.Dispose();
             }
-        
             base.Dispose(disposing);
-
-            if (logger != null)
-            {
-                logger.Log("Controller disposed");
-                logger.Dispose();
-            }
         }
     }
 }
