f3334d1de8b54c6da516b73de8357aa116fc54e1
Alexandr Djundiet <adjundiet@gmail.com>
2018-05-16 14:06:29 +0300

    Included and configured IoC container Autofac

---
+++ OnlineBookLibrary/Util/AutofacConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+using OnlineBookLibrary.Persistence.Repositories;
+
+namespace OnlineBookLibrary.Util
+{
+    public class AutofacConfig
+    {
+
+        public static void ConfigureContainer()
+        {
+            // получаем экземпляр контейнера
+            var builder = new ContainerBuilder();
+
+            // регистрируем контроллер в текущей сборке
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            // регистрируем сопоставление типов
+            builder.RegisterType<BookRepository>().As<IBookRepository>();
+            builder.RegisterType<LoanRepository>().As<ILoanRepository>();
+
+            // создаем новый контейнер с теми зависимостями, которые определены выше
+            var container = builder.Build();
+
+            // установка сопоставителя зависимостей
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
