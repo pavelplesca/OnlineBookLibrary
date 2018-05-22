88141aa51134a1fc6c27d2703d98b94b9d94b0dd
Cristian Nirones <cristian.nirones@ati.utm.md>
2018-05-22 08:36:25 +0300

    Couple tests for LoanController methods

--- OnlineBookLibrary.Tests/Controllers/LoanControllerTests.cs
+++ OnlineBookLibrary.Tests/Controllers/LoanControllerTests.cs
@@ -28,7 +28,6 @@ namespace OnlineBookLibrary.Tests.Controllers
         [Test]
         public void Index_Returns_RedirectToRouteResult()
         {
-            loanController = new LoanController(loanMock.Object);
             RedirectToRouteResult redirect = loanController.Index() as RedirectToRouteResult;
 
             Assert.That(redirect, Is.Not.Null);
@@ -49,6 +48,103 @@ namespace OnlineBookLibrary.Tests.Controllers
             var result = loanController.DisplayHistory("1");
 
             Assert.That(result, Is.TypeOf(typeof(PartialViewResult)));
+        }
+
+        [Test]
+        public void DisplayActiveLoan_Returns_PartialViewResult_If_ActiveLoan_Is_Empty()
+        {
+            loanMock.Setup(x => x.ReturnActiveLoan("1")).Returns<Loan>(null);
+
+            var result = loanController.DisplayActiveLoan("1") as PartialViewResult;
+
+            Assert.That(result, Is.TypeOf(typeof(PartialViewResult)));
+        }
+
+        [Test]
+        public void DisplayActiveLoan_Returns_PartialViewResult_If_ActiveLoan_Is_Not_Empty()
+        {
+            loanMock.Setup(x => x.ReturnActiveLoan("1")).Returns(new Loan());
+
+            var result = loanController.DisplayActiveLoan("1") as PartialViewResult;
+
+            Assert.That(result, Is.TypeOf(typeof(PartialViewResult)));
+        }
+
+        [Test]
+        public void CheckIfUserRentsBook_Returns_PartialViewResult_When_User_Doesnt_Have_An_Active_Loan_Book_Available()
+        {
+            loanMock.Setup(x => x.UserHasActiveRent("1")).Returns(false);
+            Book book = new Book { Status = BookStatus.Available };
+
+            var result = loanController.CheckIfUserRentsBook("1", book) as PartialViewResult;
+
+            Assert.That(result, Is.TypeOf<PartialViewResult>());
+        }
+
+        [Test]
+        public void CheckIfUserRentsBook_Returns_EmptyResult_When_User_Doesnt_Have_An_Active_Loan_Book_Rented()
+        {
+            loanMock.Setup(x => x.UserHasActiveRent("1")).Returns(false);
+            Book book = new Book { Status = BookStatus.Rented };
+
+            var result = loanController.CheckIfUserRentsBook("1", book) as EmptyResult;
+
+            Assert.That(result, Is.TypeOf<EmptyResult>());
+        }
+
+        [Test]
+        public void CheckIfUserRentsBook_Returns_EmptyResult_When_User_Has_An_Active_Loan_Book_Rented()
+        {
+            loanMock.Setup(x => x.UserHasActiveRent("1")).Returns(true);
+            Book book = new Book { Status = BookStatus.Rented };
+
+            var result = loanController.CheckIfUserRentsBook("1", book) as EmptyResult;
+
+            Assert.That(result, Is.TypeOf<EmptyResult>());
+        }
+
+        [Test]
+        public void CheckIfUserRentsBook_Returns_EmptyResult_When_User_Has_An_Active_Loan_Book_Available()
+        {
+            loanMock.Setup(x => x.UserHasActiveRent("1")).Returns(true);
+            Book book = new Book { Status = BookStatus.Available };
+
+            var result = loanController.CheckIfUserRentsBook("1", book) as EmptyResult;
+
+            Assert.That(result, Is.TypeOf<EmptyResult>());
+        }
+
+        [Test]
+        public void CheckCurrentLoan_Returns_PartialViewResult_When_User_Has_Active_Rent_and_CurrentLoan_Violated()
+        {
+            loanMock.Setup(x => x.UserHasActiveRent("1")).Returns(true);
+            loanMock.Setup(z => z.IsCurrentLoanViolated("1")).Returns(true);
+
+            var result = loanController.CheckCurrentLoan("1") as PartialViewResult;
+
+            Assert.That(result, Is.TypeOf<PartialViewResult>());
+        }
+
+        [Test]
+        public void CheckCurrentLoan_Returns_EmptyResult_When_User_Has_Active_Rent_and_CurrentLoan_Not_Violated()
+        {
+            loanMock.Setup(x => x.UserHasActiveRent("1")).Returns(true);
+            loanMock.Setup(z => z.IsCurrentLoanViolated("1")).Returns(false);
+
+            var result = loanController.CheckCurrentLoan("1") as EmptyResult;
+
+            Assert.That(result, Is.TypeOf<EmptyResult>());
+        }
+
+        [Test]
+        public void CheckCurrentLoan_Returns_EmptyResult_When_User_Has_No_Active_Rent_and_CurrentLoan_Not_Violated()
+        {
+            loanMock.Setup(x => x.UserHasActiveRent("1")).Returns(false);
+            loanMock.Setup(z => z.IsCurrentLoanViolated("1")).Returns(false);
+
+            var result = loanController.CheckCurrentLoan("1") as EmptyResult;
+
+            Assert.That(result, Is.TypeOf<EmptyResult>());
         }
     }
 }
