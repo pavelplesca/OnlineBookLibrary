f8937d8e5d4fc84461e966d9b25075fc097fc020
Nichita c <nichitacebotari0@gmail.com>
2018-06-01 11:53:02 +0300

    Add tag form now sends and receives json.

--- OnlineBookLibrary/Controllers/LibrarianTag.cs
+++ OnlineBookLibrary/Controllers/LibrarianTag.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Data.Entity.Core;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -28,23 +29,39 @@ namespace OnlineBookLibrary.Controllers
         }
 
         [HttpPost]
-        [ValidateAjax]
         public ActionResult AddTag(Tag tag)
         {
             var tags = bookRepository.GetTags();
             if (tags.Any(x => x.Name.ToUpper() == tag.Name?.ToUpper()))
                 ModelState.AddModelError("Name", "This Tag already exists");
-            
+            if (ModelState.IsValid)
+            {
                 var newTag = new Tag()
                 {
                     Name = tag.Name
                 };
-                
-                    bookRepository.AddTag(newTag);
+
+                bookRepository.AddTag(newTag);
                 return Json(new {Success = true});
-            
-            
-           
+            }
+
+            var errorModel =
+                    from x in ModelState.Keys
+                    where ModelState[x].Errors.Count > 0
+                    select new
+                    {
+                        key = x,
+                        errors = ModelState[x].Errors.
+                            Select(y => y.ErrorMessage).
+                            ToArray()
+                    };
+
+            var result = new JsonResult()
+            {
+                Data = errorModel
+            };
+            Response.StatusCode = 301;
+            return result;
         }
 
         [HttpPost]
